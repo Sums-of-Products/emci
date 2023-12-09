276047cf0dcf7b63fe8fd05f576696ecd34c74f5
Alex Jilkin <jilkinalex@gmail.com>
2023-12-09 22:15:57 +0200

    Improve visualisation

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# emci
-Structure MCMC
+# EMCi: Structure MCMC Discovery Tool
+
+## Description
+EMCi is a Python tool for performing Structural MCMC (Markov Chain Monte Carlo) discovery on score files. It leverages graph manipulation and sampling techniques to analyze and visualize score data, providing insights into the underlying structure.
+
+## Installation
+To install EMCi, you will need Python installed on your system along with a few dependencies. You can install all required dependencies by running:
+
+`pip install -r requirements.txt`
+
+## Dependencies
+- numpy
+- python-igraph
+- scipy
+- matplotlib
+
+## Usage
+To use EMCi, run the `main.py` script with a score file name and the number of steps. For example:
+`python3 main.py insurance-1000 5000`
+This command will process the score file `insurance-1000` with a parameter value of `5000`.
+
+## Contributing
+Contributions to EMCi are welcome. Please ensure that your code adheres to the project's style and standards.
+
+## License
+This project is licensed under GPL-3.0 license.
--- main.py
+++ main.py
@@ -4,34 +4,50 @@ from matplotlib import pyplot as plt
 import numpy as np
 from src.probabilities import ScoreManager
 from src.sampling import sample
+import sys
 
 sns.set(style="whitegrid")
-plt.figure(figsize=(8, 4))
 
-score_name = 'insurance-600'
+if len(sys.argv) < 3:
+    print("Usage: python main.py <score name> <n>")
+    sys.exit(1)
+
+score_name = sys.argv[1]
+
+try:
+    n = int(sys.argv[2])
+except ValueError:
+    print("Error: sys.argv[2] (n) is not an integer")
+    sys.exit(1)
+
 score_manager = ScoreManager(score_name)
-n = 20000
-x = 5
 
 # Initial graph
 G = ig.Graph(directed=True)
 G.add_vertices(len(score_manager.scores))
 
-# Plot KDE of simple struct n*x more samples
-for i in range(0, 2):
-    steps = sample(G, n*x, [], score_manager)
-    scores = [step[1] for step in steps][-20000:]
-    sns.kdeplot(scores, color="blue", label="Struct", fill=True)
+fig, (ax_main, ax_kde) = plt.subplots(nrows=1, ncols=2,
+                                      gridspec_kw={'width_ratios': [4, 1]}, figsize=(8, 4))
 
-# Plot KDE struct with REV
+ax_main.set_xlabel('Index')
+ax_main.set_ylabel('Scores')
+ax_kde.set_xlabel('Density')
+ax_kde.set_ylabel('')
+ax_kde.get_yaxis().set_visible(False)
+
+# Plot
+for i in range(0, 2):
+    steps = sample(G, n, [], score_manager)
+    scores = [step[1] for step in steps][len(steps)//5:]
+    ax_main.plot(range(len(scores)), scores, color='blue')
+    sns.kdeplot(scores, ax=ax_kde, vertical=True, color='blue', fill=True)
+
+# Plot with REV
 for i in range(0, 2):
     steps = sample(G, n, ['rev'], score_manager)
-    scores = [step[1] for step in steps][-20000:]
-    sns.kdeplot(scores[len(scores)//2:], color="green",
-                label="Struct w/ REV", fill=True)
+    scores = [step[1] for step in steps][len(steps)//5:]
+    ax_main.plot(range(len(scores)), scores, color='green')
+    sns.kdeplot(scores, ax=ax_kde, vertical=True, color='green', fill=True)
 
-plt.xlabel('')
-plt.ylabel('Score')
-plt.title(f'{score_name}')
-plt.legend()
+
 plt.show()
