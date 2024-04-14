Fix things and add edge probabilities calculation

main.py
@@ -1,14 +1,16 @@
 import igraph as ig
 import seaborn as sns
+import numpy as np
 from matplotlib import pyplot as plt
 from src.partition_mcmc import partition_mcmc
 from src.utils import ScoreManager
 from src.mcmc import mcmc
 import sys
 import warnings
-warnings.filterwarnings('ignore')
 
-sns.set(style="whitegrid")
+warnings.filterwarnings("ignore")
+
+sns.set_theme(style="whitegrid")
 
 if len(sys.argv) < 3:
     print("Usage: python main.py <score name> <n>")
@@ -25,40 +27,53 @@ except ValueError:
 score_manager = ScoreManager(score_name)
 
 # Initial graph
-G = ig.Graph(directed=True)
-G.add_vertices(len(score_manager.scores))
+emptyG = ig.Graph(directed=True)
+emptyG.add_vertices(len(score_manager.scores))
 
-# fig, (ax_main, ax_kde) = plt.subplots(nrows=1, ncols=2,
-#                                       gridspec_kw={'width_ratios': [4, 1]}, figsize=(8, 4))
+fig, (ax_main, ax_kde) = plt.subplots(
+    nrows=1, ncols=2, gridspec_kw={"width_ratios": [4, 1]}, figsize=(8, 4)
+)
 
-# ax_main.set_xlabel('Index')
-# ax_main.set_ylabel('Scores')
-# ax_kde.set_xlabel('Density')
-# ax_kde.set_ylabel('')
-# ax_kde.get_yaxis().set_visible(False)
+ax_main.set_xlabel("Index")
+ax_main.set_ylabel("Scores")
+ax_kde.set_xlabel("Density")
+ax_kde.set_ylabel("")
+ax_kde.get_yaxis().set_visible(False)
 
-colors = ['blue', 'green', 'red', 'magenta']
-variations = [[], ['rev']]
+
+def calculate_edge_ratios(G):
+    n = G[0].vcount()
+    cumulative_matrix = np.zeros((n, n))
+
+    for graph in G:
+        cumulative_matrix += np.array(graph.get_adjacency().data)
+
+    ratio_matrix = cumulative_matrix / len(G)
+
+    return np.round(ratio_matrix, decimals=2)
+
+
+colors = ["blue", "green", "red", "magenta"]
+variations = [[], ["rev"]]
 # Plot
 for i in range(0, 2):
-    sample_generator = mcmc(G, n, [], score_manager, 1, True)
-    steps = list(sample_generator)
+    sample_generator = mcmc(emptyG, n, ["rev"], score_manager, 1, True)
+    G, score = zip(*sample_generator)
 
-    scores = [step[1] for step in steps][-len(steps)//2:]
-    plt.hist(scores, label="Struct", range=[-22483, -22480],  alpha=0.5)
-    # ax_main.plot(range(len(scores)), scores, color='blue', label="Structural")
-    # sns.kdeplot(scores, ax=ax_kde, vertical=True, color='blue', fill=True)
+    print(calculate_edge_ratios(G[len(G) // 2 :]))
+    # plt.hist(chain[:0], label="Struct", range=[-22483, -22480], alpha=0.5)
+    ax_main.plot(range(len(score)), score, color="blue", label="Structural")
+    sns.kdeplot(score, ax=ax_kde, vertical=True, color="blue", fill=True)
 
-# Plot with REV
-for i in range(0, 2):
-    sample_generator = mcmc(G, n, ['rev'], score_manager, 1, True)
-    steps = list(sample_generator)
+# # Plot with REV
+# for i in range(0, 2):
+#     sample_generator = mcmc(G, n, ["rev"], score_manager, 1, True)
+#     steps = list(sample_generator)
 
-    scores = [step[1] for step in steps][-len(steps)//2:]
-    plt.hist(scores, label="w/ Rev", range=[-22483, -22480], alpha=0.5)
-    # ax_main.plot(range(len(scores)), scores,
-    #              color='green', label="Structural w/ REV")
-    # sns.kdeplot(scores, ax=ax_kde, vertical=True, color='green', fill=True)
+#     scores = [step[1] for step in steps]
+#     # plt.hist(scores, label="w/ Rev", range=[-22483, -22480], alpha=0.5)
+#     ax_main.plot(range(len(scores)), scores, color="green", label="Structural w/ REV")
+#     sns.kdeplot(scores, ax=ax_kde, vertical=True, color="green", fill=True)
 
 
 # for i in range(0, 2):

src/mcmc.py
@@ -2,21 +2,31 @@ import numpy as np
 from src.steps.REV import REV
 import igraph as ig
 from tqdm import tqdm
-
+from typing import Iterator
 from src.utils import R, ScoreManager
 import random
 
 
-def mcmc(G: ig.Graph, N: int, additional_steps: list[str], score_manager: ScoreManager, beta=1, show_progress=False):
-    """ Generator. 
-        yields (Graph, score)
+def mcmc(
+    G: ig.Graph,
+    N: int,
+    additional_steps: list[str],
+    score_manager: ScoreManager,
+    beta=1,
+    show_progress=False,
+) -> Iterator[tuple[ig.Graph, float]]:
+    """Generator.
+    yields (Graph, score)
     """
     G_i: ig.Graph = G.copy()
 
-    is_REV = 'rev' in additional_steps
+    is_REV = "rev" in additional_steps
 
-    pbar = tqdm(
-        range(N), bar_format='{desc}: {bar}') if show_progress else iter(lambda: True, None)
+    pbar = (
+        tqdm(range(N), bar_format="{desc}: {bar}")
+        if show_progress
+        else iter(lambda: True, None)
+    )
 
     for i in pbar:
         G_i_plus_1, step_type = propose_next(G_i, is_REV, score_manager)
@@ -27,17 +37,16 @@ def mcmc(G: ig.Graph, N: int, additional_steps: list[str], score_manager: ScoreM
         # Temperature
         likelihood_i_p_1 *= beta
 
-        if (step_type == 'REV'):
+        if step_type == "REV":
             G_i, likelihood_i, prior_i = G_i_plus_1, likelihood_i_p_1, prior_i_p_1
         else:
-            A = np.min(
-                [1, R(likelihood_i, likelihood_i_p_1, prior_i, prior_i_p_1)])
-            if (np.random.uniform() <= A):
+            A = np.min([1, R(likelihood_i, likelihood_i_p_1, prior_i, prior_i_p_1)])
+            if np.random.uniform() <= A:
                 G_i, likelihood_i, prior_i = G_i_plus_1, likelihood_i_p_1, prior_i_p_1
 
         score = likelihood_i + prior_i
-        if (show_progress):
-            pbar.set_description(f'Score: {score:.2f}')
+        if show_progress:
+            pbar.set_description(f"Score: {score:.2f}")
 
         yield G_i, score
 
@@ -48,21 +57,21 @@ def propose_next(G_i: ig.Graph, is_REV, score_manager: ScoreManager):
 
     new_edge_reversal_move = REV(score_manager).new_edge_reversal_move
 
-    if (is_REV and np.random.uniform() < 0.07):
+    if is_REV and np.random.uniform() < 0.07:
         return new_edge_reversal_move(G_i_plus_1)
 
-    if (G_i.are_connected(a, b)):
+    if G_i.are_connected(a, b):
         G_i_plus_1.delete_edges([(a, b)])
-        return G_i_plus_1, 'remove'
-    elif (G_i.are_connected(b, a)):
+        return G_i_plus_1, "remove"
+    elif G_i.are_connected(b, a):
         G_i_plus_1.delete_edges([(b, a)])
         G_i_plus_1.add_edges([(a, b)])
 
         if G_i_plus_1.is_dag():
-            return G_i_plus_1, 'reverse'
+            return G_i_plus_1, "reverse"
     else:
         G_i_plus_1.add_edges([(a, b)])
         if G_i_plus_1.is_dag():
-            return G_i_plus_1, 'add'
+            return G_i_plus_1, "add"
 
     return G_i, False

src/partition_mcmc.py
@@ -1,6 +1,5 @@
 import random
 import igraph as ig
-from utils import plot
 import itertools
 from functools import reduce, cache
 import numpy as np
@@ -14,13 +13,15 @@ from src.utils import ScoreManager
 max_parents_size = 3
 
 
-def partition_mcmc(G: ig.Graph, N, additional_steps, score_manager: ScoreManager, show_progress=False):
-    """ Generator. 
-        yields (Partition, score), (Graph, score)
+def partition_mcmc(
+    G: ig.Graph, N, additional_steps, score_manager: ScoreManager, show_progress=False
+):
+    """Generator.
+    yields (Partition, score), (Graph, score)
     """
 
-    is_MES = 'mes' in additional_steps
-    is_REV = 'rev' in additional_steps
+    is_MES = "mes" in additional_steps
+    is_REV = "rev" in additional_steps
 
     REV_move = REV(score_manager).new_edge_reversal_move
 
@@ -30,19 +31,22 @@ def partition_mcmc(G: ig.Graph, N, additional_steps, score_manager: ScoreManager
 
     G_i = G
 
-    pbar = tqdm(
-        range(N), bar_format='{desc}: {bar}') if show_progress else iter(lambda: True, None)
+    pbar = (
+        tqdm(range(N), bar_format="{desc}: {bar}")
+        if show_progress
+        else iter(lambda: True, None)
+    )
 
     for i in pbar:
         skip = False
 
-        if (is_REV and np.random.uniform() < 0.15):
+        if is_REV and np.random.uniform() < 0.15:
             m_i = len(A_i)
             G_i = sample_dag(A_i, len(G.vs), score_manager)
             G_i, type = REV_move(G_i)
 
             # Markov Equivalence step runs every time REV is sampled
-            if (is_MES):
+            if is_MES:
                 G_i, AMOs = MES(G_i)
 
             A_i = create_pratition(G_i)
@@ -53,10 +57,9 @@ def partition_mcmc(G: ig.Graph, N, additional_steps, score_manager: ScoreManager
         elif np.random.uniform() < 0.01:
             skip = True
         else:
-            A_i_p_1, scores_p_1 = sample_partition(
-                A_i, scores, n, score_manager)
+            A_i_p_1, scores_p_1 = sample_partition(A_i, scores, n, score_manager)
 
-        if (not skip):
+        if not skip:
             m_i = len(A_i)
             m_i_p_1 = len(A_i_p_1)
 
@@ -66,26 +69,30 @@ def partition_mcmc(G: ig.Graph, N, additional_steps, score_manager: ScoreManager
             # print(f'{i} {proposed_score} {A_i_p_1}')
 
             score_delta = proposed_score - score
-            if (score_delta > 250):
+            if score_delta > 250:
                 A = 1
             else:
-                R = (nbd(A_i, m_i) / nbd(A_i_p_1, m_i_p_1)) * \
-                    np.exp(score_delta)
+                R = (nbd(A_i, m_i) / nbd(A_i_p_1, m_i_p_1)) * np.exp(score_delta)
                 A = np.min([1, R])
 
             if np.random.uniform() < A:
                 A_i = A_i_p_1
                 scores = scores_p_1
 
-        if (show_progress):
-            pbar.set_description(f'Score: {sum(scores.values()):.2f}')
+        if show_progress:
+            pbar.set_description(f"Score: {sum(scores.values()):.2f}")
 
         likelihood, prior = score_manager.get_score(G_i)
         score = likelihood + prior
         yield G_i, score
 
 
-def sample_partition(prev_partitions: list[set], prev_scores: dict[int, float], n: int, score_manager: ScoreManager):
+def sample_partition(
+    prev_partitions: list[set],
+    prev_scores: dict[int, float],
+    n: int,
+    score_manager: ScoreManager,
+):
     partitions = copy.deepcopy(prev_partitions)
     scores: dict[int, float] = copy.deepcopy(prev_scores)
 
@@ -94,16 +101,16 @@ def sample_partition(prev_partitions: list[set], prev_scores: dict[int, float], 
     j = random.randint(1, nbd(partitions, m))
     vertices_to_rescore = set()
 
-    if (j < m):
+    if j < m:
         # Join partitions
         partition_1 = partitions.pop(j - 1)
         partition_2 = partitions.pop(j - 1)
 
         new_partition = partition_1 | partition_2
-        partitions.insert(j-1, new_partition)
+        partitions.insert(j - 1, new_partition)
         vertices_to_rescore |= partition_1
-        if j-2 >= 0:
-            vertices_to_rescore |= partitions[j-2]
+        if j - 2 >= 0:
+            vertices_to_rescore |= partitions[j - 2]
     else:
         # Split partition
         i_min = find_i_min(partitions, j)
@@ -113,7 +120,7 @@ def sample_partition(prev_partitions: list[set], prev_scores: dict[int, float], 
         partitions.insert(i_min - 1, new_partition)
 
         vertices_to_rescore |= new_partition
-        if (i_min - 2 >= 0):
+        if i_min - 2 >= 0:
             vertices_to_rescore |= partitions[i_min - 2]
 
     for v in vertices_to_rescore:
@@ -127,21 +134,23 @@ def create_pratition(G: ig.Graph) -> list[set]:
     partitions = []
 
     for v in G_t.vs:
-        v['original_index'] = v.index
+        v["original_index"] = v.index
 
     while len(G_t.vs) > 0:
-        outpoints = [v for v in G_t.vs if G_t.degree(v, mode='in') == 0]
-        partitions.insert(0, set([v['original_index'] for v in outpoints]))
+        outpoints = [v for v in G_t.vs if G_t.degree(v, mode="in") == 0]
+        partitions.insert(0, set([v["original_index"] for v in outpoints]))
 
         G_t.delete_vertices(outpoints)
 
     return partitions
 
 
-def P_v(partitions: list[set], v: int, n, score_manager: ScoreManager, parent_sets=False):
+def P_v(
+    partitions: list[set], v: int, n, score_manager: ScoreManager, parent_sets=False
+):
     # flat_vertices = itertools.chain.from_iterable(partitions)
 
-    if (not parent_sets):
+    if not parent_sets:
         parent_sets = get_permissible_parent_sets(partitions, v)
 
     scores = [score_manager.get_local_score(v, pa, n) for pa in parent_sets]
@@ -166,8 +175,9 @@ def get_permissible_parent_sets(partitions: list[set], v: int):
         return [frozenset()]
 
     # Get the vertices to the right of the current vertex's partition.
-    vertices_to_right = list(itertools.chain.from_iterable(
-        partitions[partition_index + 1:]))
+    vertices_to_right = list(
+        itertools.chain.from_iterable(partitions[partition_index + 1 :])
+    )
 
     # Generator to yield permissible parent sets
     def parent_sets_generator():
@@ -183,7 +193,9 @@ def get_permissible_parent_sets(partitions: list[set], v: int):
     return parent_sets
 
 
-def P_partition(partitions: list[set], n: int, score_manager: ScoreManager) -> list[float]:
+def P_partition(
+    partitions: list[set], n: int, score_manager: ScoreManager
+) -> list[float]:
     flat_vertices = list(itertools.chain.from_iterable(partitions))
 
     scores = {v: P_v(partitions, v, n, score_manager) for v in flat_vertices}
@@ -191,7 +203,17 @@ def P_partition(partitions: list[set], n: int, score_manager: ScoreManager) -> l
 
 
 def nbd_sum(partitions: tuple[set], m: int):
-    return sum([sum([binom(len(partitions[i - 1]), c) for c in range(1, len(partitions[i - 1]))]) for i in range(1, m + 1)])
+    return sum(
+        [
+            sum(
+                [
+                    binom(len(partitions[i - 1]), c)
+                    for c in range(1, len(partitions[i - 1]))
+                ]
+            )
+            for i in range(1, m + 1)
+        ]
+    )
 
 
 def nbd(partitions: list[set], m: int):
@@ -217,9 +239,12 @@ def find_c_min(partitions: list[set], j, i_min):
     c_min = 0
     while res < j:
         c_min += 1
-        res = m - 1 + base_sum + \
-            sum([binom(len(partitions[i_min - 1]), c)
-                for c in range(1, c_min + 1)])
+        res = (
+            m
+            - 1
+            + base_sum
+            + sum([binom(len(partitions[i_min - 1]), c) for c in range(1, c_min + 1)])
+        )
 
     return c_min
 
@@ -239,7 +264,8 @@ def sample_dag(partitions: list[set], n: int, score_manager: ScoreManager):
 
         for pa_i in parent_sets:
             current_sum = np.logaddexp(
-                current_sum, score_manager.get_local_score(v, pa_i, n))
+                current_sum, score_manager.get_local_score(v, pa_i, n)
+            )
 
             if current_sum >= j:
                 edges = [(p, v) for p in pa_i]

src/steps/MES/sample.py
@@ -3,8 +3,7 @@ import random
 import igraph as ig
 import networkx as nx
 import numpy as np
-from markov_equivalent.count import C, FP, count, get_maximal_cliques, v_func
-from utils import plot
+from .count import C, FP, count, get_maximal_cliques, v_func
 import matplotlib.pyplot as plt
 
 
@@ -18,12 +17,11 @@ def get_markov_equivalent_topological_orders(U: nx.Graph):
         maximal_cliques = get_maximal_cliques(clique_tree)
         r = maximal_cliques[0]
 
-        if (len(maximal_cliques) == 1 and len(maximal_cliques[0]) == 1):
+        if len(maximal_cliques) == 1 and len(maximal_cliques[0]) == 1:
             return r
 
         # Maximal clique drawn with probability proportional to v_func
-        p = list(map(lambda v: v_func(
-            UCCG, r, v, clique_tree) / AMO, maximal_cliques))
+        p = list(map(lambda v: v_func(UCCG, r, v, clique_tree) / AMO, maximal_cliques))
         v = maximal_cliques[np.random.choice(np.arange(len(p)), p=p)]
 
         K = set(v)
@@ -37,7 +35,7 @@ def get_markov_equivalent_topological_orders(U: nx.Graph):
         while is_forbidden_to:
             to = random.choice(permutations)
 
-            is_start_with_fp = [np.array_equal(to[:len(fp)], fp) for fp in FPs]
+            is_start_with_fp = [np.array_equal(to[: len(fp)], fp) for fp in FPs]
             is_forbidden_to = any(is_start_with_fp)
 
         for H in C(UCCG, K):
@@ -74,16 +72,19 @@ def MES(G: ig.Graph) -> ig.Graph:
     equivalent_G.add_vertices(len(G.vs))
 
     for to in tos:
-        for (source, target) in U.edges:
-            if (source not in to or target not in to):
+        for source, target in U.edges:
+            if source not in to or target not in to:
                 continue
-            if (to.index(source) < to.index(target)):
+            if to.index(source) < to.index(target):
                 equivalent_G.add_edge(source, target)
             else:
                 equivalent_G.add_edge(target, source)
 
     for e in G.es:
-        if not (equivalent_G.are_connected(e.source, e.target) or equivalent_G.are_connected(e.target, e.source)):
+        if not (
+            equivalent_G.are_connected(e.source, e.target)
+            or equivalent_G.are_connected(e.target, e.source)
+        ):
             equivalent_G.add_edge(e.source, e.target)
 
     return equivalent_G, AMOs
@@ -102,10 +103,8 @@ def test_top_orders_distribution(G):
     for i in range(10000):
         tos, AMOs = get_markov_equivalent_topological_orders(U)
         all_tos.append(tos)
-    all_tos = list(map(lambda tos: tuple(
-        item for t in tos for item in t), all_tos))
-    all_tos = list(map(lambda tos: ''.join(str(item)
-                   for item in tos), all_tos))
+    all_tos = list(map(lambda tos: tuple(item for t in tos for item in t), all_tos))
+    all_tos = list(map(lambda tos: "".join(str(item) for item in tos), all_tos))
 
     data = {}
     for to in all_tos:
@@ -137,20 +136,20 @@ def is_strongly_protected(G: ig.Graph, G_lines: ig.Graph, e: ig.Edge):
     # c
     b_parents = list(G.predecessors(b))
     for c in b_parents:
-        if (c != a and G.are_connected(a, c)):
+        if c != a and G.are_connected(a, c):
             return True
     # d
 
     a_neighbors = list(G.neighbors(a))
     for c1, c2 in itertools.combinations(a_neighbors, 2):
-        if (G.are_connected(c1, b) and G.are_connected(c2, b)):
+        if G.are_connected(c1, b) and G.are_connected(c2, b):
             return True
 
-    if (len(G_lines.es) == 0):
+    if len(G_lines.es) == 0:
         return False
     a_lines_neighbors = list(G_lines.neighbors(a))
     for c1, c2 in itertools.combinations(a_lines_neighbors, 2):
-        if (G.are_connected(c1, b) and G.are_connected(c2, b)):
+        if G.are_connected(c1, b) and G.are_connected(c2, b):
             return True
 
     return False
@@ -164,7 +163,7 @@ def CPDAG(D: ig.Graph) -> (ig.Graph, ig.Graph):
 
     G_i_plus_1 = undirect_non_strongly_protected_arrows(G_i, G_lines)
 
-    while (len(G_i.es) != len(G_i_plus_1.es)):
+    while len(G_i.es) != len(G_i_plus_1.es):
         G_i = G_i_plus_1.copy()
         G_i_plus_1 = undirect_non_strongly_protected_arrows(G_i, G_lines)
 